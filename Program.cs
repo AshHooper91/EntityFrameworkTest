318de4c0e87a42a1f8de9f0b399313ba36f740c2
Ashley Hooper <ashleyhooper@blueyonder.co.uk>
2019-04-27 18:08:02 +0100

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EntityFrameWorkTutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
